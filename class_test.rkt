40de871c43645a9c7ac105b08e9ecf59cca2a338
Alec Brunelle <alec@alec.coffee>
2016-11-03 15:15:51 -0400

    1.2 working with one trait

--- class_test.rkt
+++ class_test.rkt
@@ -55,7 +55,7 @@
         (map first (p "_methods")))
       '("black" "white"))
 
-#|
+
 ; Traits for testing
 (define (distance-trait obj)
   (lambda (msg)
@@ -69,11 +69,11 @@
                  obj2))]
           [else (obj msg)])))
 
-(define (distance-trait-2 obj)
-  (lambda (msg)
-    (cond [(equal? msg "distance-plus")
-           (lambda (other x) (+ ((obj "distance") other) x))]
-          [else (obj msg)])))
+;(define (distance-trait-2 obj)
+;  (lambda (msg)
+;    (cond [(equal? msg "distance-plus")
+;           (lambda (other x) (+ ((obj "distance") other) x))]
+;          [else (obj msg)])))
 
 ; Tests for Question 2
 
@@ -83,11 +83,11 @@
          [dy (- y (other-point "y"))])
      (sqrt (+ (* dx dx) (* dy dy))))])
 
-(class-trait Point2 (x y) (with distance-trait distance-trait-2)
-  [(distance other-point)
-   (let ([dx (- x (other-point "x"))]
-         [dy (- y (other-point "y"))])
-     (sqrt (+ (* dx dx) (* dy dy))))])
+;(class-trait Point2 (x y) (with distance-trait distance-trait-2)
+;  [(distance other-point)
+;   (let ([dx (- x (other-point "x"))]
+;         [dy (- y (other-point "y"))])
+;     (sqrt (+ (* dx dx) (* dy dy))))])
 
 (test (let* ([p1 (Point 2 3)])
         (list (p1 "x") (p1 "y")))
@@ -97,20 +97,19 @@
              [p2 (Point 15 40)])
         ((p1 "distance") p2))
       15)
-
+;
 (test (let* ([p1 (Point 2 3)])
         ((p1 "distance-to-self")))
       0)
-
-(test (let* ([p1 (Point 30 40)]
-             [p2 (Point 15 40)]
-             [p3 (Point 31 39)]
-             [p4 ((p1 "closer") p2 p3)])
-        (list (p4 "x") (p4 "y")))
-      '(31 39))
-
-(test (let* ([p1 (Point2 30 40)]
-             [p2 (Point 15 40)])
-        ((p1 "distance-plus") p2 20))
-      35)
-|#
+;
+;(test (let* ([p1 (Point 30 40)]
+;             [p2 (Point 15 40)]
+;             [p3 (Point 31 39)]
+;             [p4 ((p1 "closer") p2 p3)])
+;        (list (p4 "x") (p4 "y")))
+;      '(31 39))
+;
+;(test (let* ([p1 (Point2 30 40)]
+;             [p2 (Point 15 40)])
+;        ((p1 "distance-plus") p2 20))
+;      35)
